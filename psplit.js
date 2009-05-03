98d8df3656ab8fd51a27dcb6e93807a022598690
Uchimata <cheap.sheep.u@gmail.com>
2009-05-04 04:20:31 +0900

    moved "var arg_limit = limit"
    日本語使っていいものかな

--- psplit.js
+++ psplit.js
@@ -1,6 +1,6 @@
 // Perl-like split
 String.prototype.psplit = function(sep, limit) {
-	var str = this+'', arg_limit = limit;
+	var str = this+'';
 	if (typeof sep != 'string' && typeof sep != 'object'
 		|| typeof sep == 'object' && (sep.constructor+'').match(/^\s*function\s+([\w$]+)/)[1] != 'RegExp')
 			{ sep = sep+'' }
@@ -8,7 +8,7 @@ String.prototype.psplit = function(sep, limit) {
 	if (typeof limit != 'number') limit = 0;
 	if (limit == 1) return [str];
 	else {
-		var a = [], b = 0, c = 0, clear = false;
+		var a = [], b = 0, c = 0, clear = false, arg_limit = limit;
 		if (typeof sep == 'string') {
 			sep = sep.replace("\\", "\\\\");
 			if (sep == ' ') sep = /\s+/, clear = true;
